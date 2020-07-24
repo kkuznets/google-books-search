3ea32837214b11819b8a01be2b4484404e6c69ba
Kirill Kuznetsov <kirill@kkuznets.dev>
2020-07-25 03:19:33 +1000

    Updated no results message

--- client/src/pages/NoMatch/NoMatch.js
+++ client/src/pages/NoMatch/NoMatch.js
@@ -1,18 +1,16 @@
-import React from "react";
-import { Container } from "../../components/Grid";
-import Jumbotron from "../../components/Jumbotron";
+import React from 'react';
+import { Container } from '../../components/Grid';
+import Jumbotron from '../../components/Jumbotron';
 
 const NoMatch = () => (
-  <Container>
-    <Jumbotron>
-      <h1>404 Page Not Found</h1>
-      <h1>
-        <span role="img" aria-label="Face With Rolling Eyes Emoji">
-          🙄
-            </span>
-      </h1>
-    </Jumbotron>
-  </Container>
+   <Container>
+      <Jumbotron>
+         <h1>404 Page Not Found</h1>
+         <h1>
+            <span role='img' aria-label='Face With Rolling Eyes Emoji'></span>
+         </h1>
+      </Jumbotron>
+   </Container>
 );
 
 export default NoMatch;
