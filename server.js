9bd9870545afb5cfd6e95c866f9588d4d9468e41
Kirill Kuznetsov <kirill@kkuznets.dev>
2020-07-25 03:17:04 +1000

    Updated routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const routes = require("./routes");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,16 +9,14 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static('client/build'));
 }
-// Add routes, both API and view
-app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
 
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+app.listen(PORT, function () {
+   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
