2bdf7281777a068855efd44566d63b41efddf62d
Kirill Kuznetsov <kirill@kkuznets.dev>
2020-07-25 03:19:44 +1000

    Added emoji

--- client/src/pages/NoMatch/NoMatch.js
+++ client/src/pages/NoMatch/NoMatch.js
@@ -7,7 +7,9 @@ const NoMatch = () => (
       <Jumbotron>
          <h1>404 Page Not Found</h1>
          <h1>
-            <span role='img' aria-label='Face With Rolling Eyes Emoji'></span>
+            <span role='img' aria-label='Face With Rolling Eyes Emoji'>
+               🙄
+            </span>
          </h1>
       </Jumbotron>
    </Container>
